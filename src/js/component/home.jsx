4fdf1d6202016cd6fb8be59a8c59cec448d3da77
Lisa Valdes <132381289+LisaMV@users.noreply.github.com>
2023-07-03 00:41:37 +0000

    Simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faClock} from '@fortawesome/free-regular-svg-icons'
+
+
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div class='container bg-dark'>
+        <div class='row'>
+            <div class='col-4 pt-4'>
+            <img  class='image'src='https://i.pinimg.com/originals/92/13/df/9213df2e7622e767f9fd09fcd656c5cb.jpg'></img>
+			</div>
+            <div class='col-1 fs-2 pt-4 text-white'>0</div>
+            <div class='col-1 fs-2  pt-4 text-white'>0</div>
+            <div class='col-1 fs-2  pt-4 text-white'>0</div>
+            <div class='col-1 fs-2   pt-4 text-white'>{props.tens}</div>
+            <div class='col-1 fs-2   pt-4 text-white'>{props.hundreds}</div>
+            <div class='col-1 fs-2   pt-4 text-white'>{props.unit}</div>
+        </div>
+    </div>
+       
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,27 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+let unit=0;
+let hundreds =0;
+let tens =0;
+
+setInterval (()=>{
+    unit ++
+    if (unit > 9){
+        hundreds ++
+        unit = 0;
+    }
+   if (hundreds > 9){
+        tens ++
+        hundreds =0;
+    }
+
+     if (tens > 9){
+        tens =0;
+
+    }
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Home unit={unit} hundreds={hundreds} tens={tens} />, document.querySelector("#app"));
+},1000)
